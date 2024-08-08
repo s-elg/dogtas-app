3cb4f8dc388ed301b48faf9de3ef4ab3d37fbbcb
sumeyye elgin <sumeyyeelgin@hotmail.com>
2024-08-08 11:35:58 +0300

    react-native-image-zoom-viewer paketi ile görsellere zoom yapıldı

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity, Modal } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 
 import styles from './Card.style';
 
 const Card = (props) => {
+    // Modal'ın görünürlük durumunu kontrol eden state
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <View style={styles.container}>
-            <Image source={{ uri: props.product.imgURL }} style={styles.image} />
+            {/* Ürün resmini tıklanabilir hale getiren TouchableOpacity bileşeni */}
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <Image
+                    source={{ uri: props.product.imgURL }}
+                    style={styles.image}
+                />
+            </TouchableOpacity>
+            <Modal
+                visible={modalVisible} // Modal'ın görünür olup olmadığını kontrol eder
+                transparent={true} // Modal'ın arka planını şeffaf yapar
+                onRequestClose={() => setModalVisible(false)} // Donanım geri düğmesine basıldığında modal'ı kapatır
+                animationType="fade"  //resmin açılış ve kapanışında 
+            >
+                <ImageViewer
+                    imageUrls={[{ url: props.product.imgURL }]} // Resim URL'sini belirler
+                    onClick={() => setModalVisible(false)} // Modal'ı kapatır
+                />
+
+            </Modal>
             <View style={styles.info_container}>
                 <View style={styles.description_container}>
                     <View style={styles.product_info}>
@@ -27,5 +49,6 @@ const Card = (props) => {
         </View>
     )
 }
+
 
 export default Card;
